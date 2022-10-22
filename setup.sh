981f99cb2b0cce59c5a120f80988dd1a67f148a6
kpanic <anhptvolga@yandex.ru>
2022-10-22 09:00:46 +0700

    update setup

--- setup.sh
+++ setup.sh
@@ -6,53 +6,56 @@
 
 . /etc/os-release
 
-if [[ $ID != 'manjaro' || $ID_LIKE != 'arch' ]]; then
+if [[ $ID != 'manjaro' && $ID_LIKE != 'arch' ]]; then
     echo "This system ${ID} is not supported by this script!"
     echo "Just do some common setup!"
 fi
 
-install_depends() {
-    if [[ $ID = 'manjaro' ]]; then
-        devel=(base-devel gcc gdb ninja make cmake perf kdiff3 diff-so-fancy python3)
-        editor=(vim neovim)
-        media=(pulseaudio pulseaudio-alsa pulseaudio-bluetooth)
-        utils=(sudo zsh tmux tmuxp ripgrep xclip curl fzf fd jq alacritty ctags bat tldr goldendict albert muparser)
-        sudo pacman -Sy --needed ${devel[@]} ${editor[@]} ${utils[@]} ${media[@]}
-        sudo npm install -g fx
-    fi
+_in() {
+    yay -Syu --nocleanmenu --nodiffmenu --noupgrademenu --noeditmenu --noconfirm $@
+}
+
+common() {
+    echo '-> common'
+    # yay
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd -
+    rm -rf yay
+    # zimfw
+    _in zsh neovim go
+    curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+    # bamboo
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/BambooEngine/ibus-bamboo/master/archlinux/install.sh)"
+    # neovim
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 }
 
 install_python() {
-    sudo pacman -Sy --needed python3 python-virtualenv python-pylint yapf
+    _in python3 python-virtualenv python-pylint yapf
 }
 
 install_java() {
-    sudo pacman -Sy --needed jdk11-openjdk gradle maven
-    sudo archlinux-java set java-11-openjdk
+    _in gradle maven \
+        jdk17-openjdk openjdk17-doc openjdk17-src \
+        jdk11-openjdk openjdk11-src openjdk11-doc \
+        jdk8-openjdk openjdk8-doc openjdk8-src
+    sudo archlinux-java set java-17-openjdk
 }
 
 setup_tmux() {
+    _in tmux
     TMUX_TEMP=~/.tmux
     mkdir ~/.tmux
     git clone https://github.com/tmux-plugins/tmux-resurrect ${TMUX_TEMP}/resurrect
     git clone https://github.com/tmux-plugins/tmux-continuum ${TMUX_TEMP}/continuum
 }
 
-clone_repo() {
+_clone_repo() {
     mkdir -p $2
     git clone $1 $2
-}
-
-setup_common() {
-    mkdir ~/.history
-    curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-    chsh -s =zsh
-}
-
-cp_config_files() {
-    for file in .ideavimrc .config .editrc .gitattributes .gitconfig .gitignore_global .gitmsg.txt .inputrc .kutils .myclirc .tmux.conf .vim-coc .vimrc .zimrc; do
-        cp $file ~
-    done
 }
 
 setup_ssh() {
@@ -64,25 +67,44 @@ setup_ssh() {
     chmod 600 ~/.ssh/authorized_keys
 }
 
-setup_vim() {
-    echo "Clone & setup vim"
-    # vim
-    VIMCONFIG='~/.vim'
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    mkdir -p ~/.local/share/nvim/site/autoload
-    ln -s ~/.vim/autoload/plug.vim ~/.local/share/nvim/site/autoload/plug.vim
-}
-
-setup_cpp_tdd() {
-    echo "Setup for c++ TDD..."
-    # google test
-    # google benchmark
+setup_cli_tools() {
+    _in base-devel gcc gdb ninja make cmake perf python3 sudo ctags \
+         ripgrep xclip curl fzf fd jq bat dex \
+        alacritty ctags tldr \
+        fash unclutter yq \
+        github-cli pgcli mycli
+    sudo npm install -g fx
 }
 
 setup_tools() {
-    TOOLS_HOME=~/.devtools
+    _in unicode-emoji tff-hack \
+        rofi \
+        libinput-gestures wmctrl xdotool autorandr \
+        docker docker-compose \
+        npm nodejs yarn \
+        postgresql \
+        blueman bluez bluez-utils \
+        sysstat dnsutils inetutils \
+        openssh openvpn networkmanager-openvpn \
+        htop zip unzip \
+        redshift \
+        acpi \
+        copyq scrot \
+        gnome-keyring \
+        foliate zathura zathura-pdf-mupdf \
+        cpupower \
+        x86_energy_perf_policy \
+        tree ranger ueberzug \
+        kubectl k9s stern \
+        jetbrains-toolbox pgmodeler \
+        telegram-desktop-bin \
+        goldendict \
+        brave-bin \
+        binance
+
+    TOOLS_HOME=~/github
     mkdir $TOOLS_HOME
-    clone_repo 'https://github.com/brendangregg/FlameGraph' $TOOLS_HOME/FlameGraph
+    _clone_repo 'https://github.com/brendangregg/FlameGraph' $TOOLS_HOME/FlameGraph
     # get .gdbinit file and customize
     wget -O ~/.gdbinit https://raw.githubusercontent.com/gdbinit/Gdbinit/master/gdbinit
     echo 'set $SHOWOBJECTIVEC = 0
@@ -92,11 +114,9 @@ set print pretty on'
     > ~/.gdbinit.local
 }
 
+common
 setup_tmux
-#setup_common
-#setup_ssh
-#cp_config_files
-#setup_vim
-#setup_cpp_tdd
-#setup_tools
+install_python
+install_java
+setup_tools
 
